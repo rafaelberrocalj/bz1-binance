e4f6edfc694eb67ab7da41732cd671fc9a90f6e8
Rafael Berrocal Justiniano <rafaelberrocalj@gmail.com>
2024-07-23 17:47:45 -0300

    validate configuration, date format, show local and universal time

--- src/Program.cs
+++ src/Program.cs
@@ -12,36 +12,60 @@ var builder = new ConfigurationBuilder()
     .AddUserSecrets(Assembly.GetExecutingAssembly());
 var configuration = builder.Build();
 
+Console.WriteLine($"bz1-binance starting");
+
+ArgumentNullException.ThrowIfNullOrWhiteSpace(configuration["API_KEY"], "API_KEY");
+ArgumentNullException.ThrowIfNullOrWhiteSpace(configuration["API_SECRET"], "API_SECRET");
+ArgumentNullException.ThrowIfNullOrWhiteSpace(configuration["START_DATE"], "START_DATE");
+
 var binanceRestClient = new BinanceRestClient(options =>
 {
-    options.ApiCredentials = new ApiCredentials(configuration["API_KEY"], configuration["API_SECRET"]);
+    options.ApiCredentials = new ApiCredentials(configuration["API_KEY"]!, configuration["API_SECRET"]!);
 });
 
 var binanceConvertTradeList = new List<Binance.Net.Objects.Models.Spot.Convert.BinanceConvertTrade>();
 
-var startDate = DateTime.Parse(configuration["START_DATE"]);
+var startDate = DateTime.Parse(configuration["START_DATE"]!);
+var dateFormat = "yyyy/MM/dd";
 
+Console.WriteLine();
 while (startDate <= DateTime.Today)
 {
     var endDate = startDate.AddDays(30);
 
-    Console.WriteLine($"listing trade history from {startDate.ToString("dd/MM/yyyy")} to {endDate.ToString("dd/MM/yyyy")}");
-
     var currentTradeHistory = await binanceRestClient.SpotApi.Trading.GetConvertTradeHistoryAsync(startDate, endDate);
     if (currentTradeHistory.Success)
+    {
         binanceConvertTradeList.AddRange(currentTradeHistory.Data.Data);
+
+        Console.WriteLine($"listing trade history from {startDate.ToString(dateFormat)} to {endDate.ToString(dateFormat)} with {currentTradeHistory.Data.Data.Count()} results");
+    }
+    else
+    {
+        Console.WriteLine($"listing trade history from {startDate.ToString(dateFormat)} to {endDate.ToString(dateFormat)} with error {currentTradeHistory.Error?.Message}");
+    }
 
     startDate = endDate;
 }
 
+var integrationPairs = new List<string>();
+
 Console.WriteLine();
-foreach (var historyItem in binanceConvertTradeList.Select(s => new { Pair = s.QuoteAsset + s.BaseAsset, s.QuoteAsset, s.BaseAsset, s.CreateTime }))
+foreach (var historyItem in binanceConvertTradeList.Select(s => new
+{
+    Pair = s.QuoteAsset + s.BaseAsset,
+    s.QuoteAsset,
+    s.BaseAsset,
+    CreateLocalTime = s.CreateTime.ToLocalTime(),
+    CreateUniversalTime = s.CreateTime.ToUniversalTime()
+}))
 {
     Console.WriteLine($"historyItem={historyItem}");
+
+    integrationPairs.Add(historyItem.Pair);
 }
 
-var integrationPairs = JsonSerializer.Serialize(binanceConvertTradeList.Select(s => s.QuoteAsset + s.BaseAsset).OrderBy(q => q).ToList());
 Console.WriteLine();
-Console.WriteLine($"integrationPair={integrationPairs}");
+Console.WriteLine($"integrationPair={JsonSerializer.Serialize(integrationPairs.Distinct().OrderBy(q => q))}");
 
 Console.ReadLine();
